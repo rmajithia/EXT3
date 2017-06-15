5cf99722e299a791957bb32d7b0f07f3afc95926
rmajithia <rohitvm2002@gmail.com>
2017-06-15 13:55:09 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-
 var menuItem = {
     "id": "createKB",
     "title": "Create KB Now",
@@ -19,17 +18,17 @@ chrome.contextMenus.create(menuItem1);
 chrome.contextMenus.create(menuItem2);
 
 function isInt(value) {
-  return !isNaN(value) && 
-         parseInt(Number(value)) == value && 
-         !isNaN(parseInt(value, 10));
+    return !isNaN(value) &&
+        parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value, 10));
 }
- 
-function fixedEncodeURI (str) {
+
+function fixedEncodeURI(str) {
     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
 }
 
-chrome.contextMenus.onClicked.addListener(function(clickData){   
-    if (clickData.menuItemId == "createKB" && clickData.selectionText){    
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+    if (clickData.menuItemId == "createKB" && clickData.selectionText) {
         /*var speakText='Creating KB Article for you...';
          chrome.tts.speak(speakText,
                         {
@@ -37,45 +36,52 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
                         });         
        
        */
-      var selectionText=clickData.selectionText;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box",selectedText:selectionText}, function(response) {});  
-    });
+        var selectionText = clickData.selectionText;
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: "open_dialog_box",
+                selectedText: selectionText
+            }, function(response) {});
+        });
 
-      
+
     }
-    if (clickData.menuItemId == "Speak" && clickData.selectionText){        
-       chrome.tts.speak(clickData.selectionText,
-                        {
-                            'rate': 0.7
-                        });         
+    if (clickData.menuItemId == "Speak" && clickData.selectionText) {
+        chrome.tts.speak(clickData.selectionText, {
+            'rate': 0.7
+        });
     }
 
-    if (clickData.menuItemId == "sNowCommunity" && clickData.selectionText){    
+    if (clickData.menuItemId == "sNowCommunity" && clickData.selectionText) {
         var wikiUrl = "https://www.google.com/#q=servicenow community+" + fixedEncodeURI(clickData.selectionText);
         var createData = {
             "url": wikiUrl,
             "type": "popup",
             "top": 5,
             "left": 5,
-            "width": screen.availWidth/2,
-            "height": screen.availHeight/2
-        };              
-        chrome.windows.create(createData, function(){});        
+            "width": screen.availWidth / 2,
+            "height": screen.availHeight / 2
+        };
+        chrome.windows.create(createData, function() {});
     }
 
 
 });
 
 
-chrome.runtime.onMessage.addListener(function (msg, sender) {
-  // First, validate the message's structure
-  if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-    // Enable the page-action for the requesting tab
-    chrome.pageAction.show(sender.tab.id);
-  }
+chrome.runtime.onMessage.addListener(function(msg, sender) {
+    // First, validate the message's structure
+    if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
+        // Enable the page-action for the requesting tab
+        chrome.pageAction.show(sender.tab.id);
+    }
 });
 
-chrome.storage.onChanged.addListener(function(changes, storageName){
-    chrome.browserAction.setBadgeText({"text": "40"});
+chrome.storage.onChanged.addListener(function(changes, storageName) {
+    chrome.browserAction.setBadgeText({
+        "text": "40"
+    });
 });
--- popup.js
+++ popup.js
@@ -1,118 +1,124 @@
 // Update the relevant fields with the new data
 function setDOMInfo(info) {
- 
-  
-  //document.getElementById('total').textContent   = info.total;
-  //document.getElementById('inputs').textContent  = info.inputs;
-  //document.getElementById('buttons').textContent = info.buttons;
-  
- //  document.getElementById('tab3').innerHTML = info.incidents;
-    // alert(info.incidents);
-     console.log(info.incidents);
-         document.getElementById('table2').innerHTML = iterateChange(info.buttons);
 
-    document.getElementById('tab3').innerHTML = iterateIncidents(info.incidents);
+
+    //document.getElementById('total').textContent   = info.total;
+    //document.getElementById('inputs').textContent  = info.inputs;
+    // alert(info.buttons);
+    // document.getElementById('buttons').textContent = info.buttons;
+
+    //  document.getElementById('tab3').innerHTML = info.incidents;
+    // alert('yyyy'+info.incidents);
+
+    document.getElementById('tab3').innerHTML = iterateChange(info.buttons);
+
+    document.getElementById('tab1').innerHTML = iterateIncidents(info.incidents);
     var notifOptions = {
-                        type: "basic",
-                        iconUrl: "icon48.png",
-                        title: "Objects Retrieved!",
-                        message: "Service now objects have been retrieved!!!!."
-                };
-                chrome.notifications.create('limitNotif', notifOptions);
+        type: "basic",
+        iconUrl: "icon48.png",
+        title: "Objects Retrieved!",
+        message: "Service now objects have been retrieved!!!!."
+    };
+    chrome.notifications.create('limitNotif', notifOptions);
 }
 
-function iterateIncidents(incidentDetails)
-{
- var str='';
-  var jsonObj=JSON.parse(incidentDetails);
-                for(var i=0;i<jsonObj.length;i++)
-                {        
-                   
-                   str+=  printIncident(jsonObj[i].number,jsonObj[i].short_description,jsonObj[i].sys_id);
-                }
+function iterateIncidents(incidentDetails) {
+    var str = ' <h2>My Priority Incidents111</h2>';
+    var jsonObj = JSON.parse(incidentDetails);
+    for (var i = 0; i < jsonObj.length; i++) {
 
-  
-       return str;
-}
-function iterateChange(changeDetails)
-{
- var str='<table data-toggle="table" class="table table-responsive"><thead><tr><th>Number</th><th>Short Description</th><th>Planned Start Date</th><th>Planned End Date</th><th>Action</th></tr></thead><tbody>';
-  var jsonObj=JSON.parse(changeDetails);
-                for(var i=0;i<jsonObj.length;i++)
-                {        
-                    
-                   str+=  printChange(jsonObj[i].ca_number,jsonObj[i].ca_short_description,jsonObj[i].cap_sys_id,jsonObj[i].ca_start_date,jsonObj[i].ca_end_date);
-                }
+        str += printIncident(jsonObj[i].number, jsonObj[i].short_description, jsonObj[i].sys_id);
+    }
 
-  
-       return str+'</tbody></table>';
+
+    return str;
 }
 
-function printChange(number,shortDescription,sys_id,plannedStartDate,plannedEndDate)
-{
+function iterateChange(changeDetails) {
+    var str = '<table data-toggle="table" class="table table-responsive"><thead><tr><th>Number</th><th>Short Description</th><th>Planned Start Date</th><th>Planned End Date</th><th>Action</th></tr></thead><tbody>';
+    var jsonObj = JSON.parse(changeDetails);
+    for (var i = 0; i < jsonObj.length; i++) {
 
-var  str=
+        str += printChange(jsonObj[i].ca_number, jsonObj[i].ca_short_description, jsonObj[i].cap_sys_id, jsonObj[i].ca_start_date, jsonObj[i].ca_end_date);
+    }
 
-    '<tr  class="tr-class-1">'+
-                '<td>'+number+'</td>'+
-                   '<td>'+shortDescription+'</td>'+
-                     ' <td>'+plannedStartDate+'</td>'+
-                   ' <td>'+plannedEndDate+'</td>'+
-				   '<td> <a href="https://dev28379.service-now.com/nav_to.do?uri=sysapproval_approver.do?sys_id='+sys_id+'" class="w3-button w3-red">Approve/Reject</a> </td>">'+
-                      '    </tr>';
-                  
+    alert(str);
+    return str + '</tbody></table>';
+}
 
- return str;
+function printChange(number, shortDescription, sys_id, plannedStartDate, plannedEndDate) {
+
+    var str =
+
+        '<tr  class="tr-class-1">' +
+        '<td>' + number + '</td>' +
+        '<td>' + shortDescription + '</td>' +
+        ' <td>' + plannedStartDate + '</td>' +
+        ' <td>' + plannedEndDate + '</td>' +
+        '<td> <a href="https://dev28379.service-now.com/nav_to.do?uri=sysapproval_approver.do?sys_id=' + sys_id + '" class="w3-button w3-red">Approve/Reject</a> </td>">' +
+        '    </tr>';
+
+
+    return str;
 }
 
 
-function printIncident(number,shortDescription,sys_id)
-{
+function printIncident(number, shortDescription, sys_id) {
 
-var  str=
+    var str =
 
-    '<div class="w3-container">'+
-                '<div class="w3-card-4"  style="width:100%; background-color:#fff;">'+
-                   '<div class="w3-container w3-center">'+
-                     ' <p class="preppy">'+number+':'+shortDescription+'</p>'+
-                   ' </div>'+
-				   '<div class="w3-container w3-center">'+
-                      '<img src="http://w3.unisa.edu.au/facilities/contractors/hazard-warning_icon_1024x1024.png" alt="Monitor" style="width:200px">'+
-                   '</div>'+
 
-				    '<div align="right">'+
-					 '</br>'+
-      '<p class="text-right"><a href="https://dev28379.service-now.com/nav_to.do?uri=incident.do?sys_id="'+sys_id+' class="w3-button w3-red">Open</a></p>'+
-                  '</div>'+
-               '</div>'+
-				 
-           ' </div></br>';
+        '<div class="row">' +
 
- return str;
+        '<div class="info-card">' +
+        '<div class="front">' +
+
+        '<div class="col-sm-12 thumbnail text-center">' +
+        ' <img alt="" class="img-responsive" src="http://pcwrangler.com.au/wp-content/uploads/2013/10/Priority-300x232.jpg">' +
+        '<div class="caption">' +
+        ' <h1>' + number + '</h1>' +
+        '  <h2>' + shortDescription + '</h2>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="back">' +
+        '<h1>' + number + ':' + shortDescription + '</h1>' +
+        '<p>' +
+        shortDescription +
+        '</p>' +
+        '<div class="social">' +
+        '<a href="#" class="social-icon facebook  animate"><span class="fa fa-facebook"></span></a>' +
+        '<a href="https://twitter.com/MichaelCanlas7" target="_blank" class=" social-icon twitter  animate"><span class="fa fa-twitter"></span></a>' +
+        '<a href="https://github.com/ironprice91" target="_blank" class=" social-icon github  animate"><span class="fa fa-github-alt"></span></a>' +
+        '</div>  </div>  </div> </div>';
+
+
+
+    //    alert(str);
+
+
+
+
+    return str;
 }
 
 // Once the DOM is ready...
-window.addEventListener('DOMContentLoaded', function () {
-  // ...query for the active tab...
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, function (tabs) {
-    // ...and send a request for the DOM info...
-    chrome.tabs.sendMessage(
-        tabs[0].id,
-        {from: 'popup', subject: 'DOMInfo'},
-        // ...also specifying a callback to be called 
-        //    from the receiving end (content script)
-        setDOMInfo);
-  });
-  
-  
-});
+window.addEventListener('DOMContentLoaded', function() {
+    // ...query for the active tab...
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        // ...and send a request for the DOM info...
+        chrome.tabs.sendMessage(
+            tabs[0].id, {
+                from: 'popup',
+                subject: 'DOMInfo'
+            },
+            // ...also specifying a callback to be called 
+            //    from the receiving end (content script)
+            setDOMInfo);
+    });
 
-$(document).ready(function(){
-   $('body').on('click', 'a', function(){
-     chrome.tabs.create({url: $(this).attr('href')});
-     return false;
-   });
+
 });
